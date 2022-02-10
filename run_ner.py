4c735a7442375f25dc1796fe90dc6a32c13d7037
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-02-10 00:37:14 +0000

    Bug fix for loop
    enumerate for index

--- run_ner.py
+++ run_ner.py
@@ -184,8 +184,8 @@ simplified_labels = { "O": "O", "B-MISC": "miscellaneous", "I-MISC": "miscellane
 def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
     """Loads a data file into a list of `InputBatch`s."""
       
-    for label in label_list:
-      label_list[label] = simplified_labels[label]
+    for i, label in enumerate(label_list):
+      label_list[i] = simplified_labels[label]
 
     label_map = {label : i for i, label in enumerate(label_list,1)}
     print(label_map)
