6168814d875927d93c785debccfa9b904ce0c618
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 04:04:12 +0000

    syntax on method string cast
    string to str

--- run_ner.py
+++ run_ner.py
@@ -221,7 +221,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
         # "O" default for labels
         for label in simp_labs:
             label_to_add = list(simplified_labels.keys())[list(simplified_labels.values()).index(label)]
-            label_in_string = string(label_to_add)
+            label_in_string = str(label_to_add)
             labellist.append(label_in_string)
         print(labellist)
 
