e51822c84b7fbaf6c450cf74ae40acd6ea7c288a
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 09:28:16 +0000

    Update run_ner.py

--- run_ner.py
+++ run_ner.py
@@ -181,7 +181,7 @@ def shuffle_label_map(labels):
     shuffle_values = list(labels.values())
     random.shuffle(shuffle_values)
     labels = dict(zip(labels, shuffle_values))
-    print(labels)
+    #print(labels)
     return labels
 
 simplified_labels = { "O": "O", "B-MISC": "miscellaneous", "I-MISC": "miscellaneous", "B-PER": "person", "I-PER": "person", 
