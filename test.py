c998181de1c4e0cca92947d13bd7e4669272be87
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 07:19:34 +0000

    Update test.py

--- test.py
+++ test.py
@@ -30,6 +30,6 @@ print("Actual Results:")
 print(output)
 
 #New method
-print("Group 4: ZER Test)
+print("Group 4: ZER Test")
 model.predict_zero_shot("Steve goes to the shop in Belfast, Apple", ["person", "location", "organisation"])
 
