47488c76b381fac253435f971fedb6395aa05499
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 04:38:09 +0000

    groups of associated words

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
 from bert import Ner
 
-model = Ner("out_base_simp/")
+model = Ner("out_base_tes/")
 
-output = model.predict("Steve went to Paris")
-
+#Persons
+print("Group 1: Persons")
+print("Expected: Final classification for person label below")
+expected = model.predict("person")
+output = model.predict("Steve John James Daniel Zendaya Robert Pattinson")
+print(expected)
+print("Actual Results:")
 print(output)
+  
+#Organisations
+print("Group 2: Organisations")
+print("Expected: Final classification for person label below")
+expected = model.predict("organisation")
+output = model.predict("Starbucks Microsoft AMD Intel Target Kellogs Walmart Disney")
+print(expected)
+print("Actual Results:")
+print(output)
+
+#Locations
+print("Group 3: Locations")
+print("Expected: Final classification for person label below")
+expected = model.predict("Locations")
+output = model.predict("Berlin New York Munich Belfast Dublin England America")
+print(expected)
+print("Actual Results:")
+print(output)
+
