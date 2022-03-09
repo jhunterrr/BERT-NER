92e754b0d21e2606780fbb7e2a31046cc33f0766
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 10:09:24 +0000

    Update run_ner.py

--- run_ner.py
+++ run_ner.py
@@ -178,8 +178,8 @@ def get_simple_labels():
     return ["O","person","organisation","location"]   
 
 def shuffle_label_map(labels):
-    shuffle_key = [*labels.keys()][-2]
-    shuffle_values = [*labels.values()][-2]
+    shuffle_key = [*labels.keys()][:-2]
+    shuffle_values = [*labels.values()][:-2]
     print(shuffle_key)
     random.shuffle(shuffle_values)
     shuffle_key.extend("[CLS]")
