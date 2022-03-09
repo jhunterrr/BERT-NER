7daf88f67f1134f7b1b040d9043e7e9e427ad5c7
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 09:01:09 +0000

    Update bert.py

--- bert.py
+++ bert.py
@@ -168,7 +168,7 @@ class Ner:
         for determined_label in after_sep:
             print("model groups these words to be common with " + str(determined_label["word"]))
             for predicted_label in before_sep:
-               if predicted_label["label"] is determined_label["label"]:
+               if predicted_label["tag"] is determined_label["tag"]:
                   print(str(predicted_label["word"]))
         
         #for chosen_label in labellist
