082bd72aab35fb33a4ee26e0b89d9847d1b504b2
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 07:07:46 +0000

    attempt

--- run_ner.py
+++ run_ner.py
@@ -204,7 +204,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
         #     labellist.extend("O")
 
         #shuffle label map
-        shuffle_label_map(labels = label_map)
+        label_map = shuffle_label_map(labels = label_map)
         
         # print(label_map) 
 
