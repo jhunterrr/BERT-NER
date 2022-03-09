52efaad9bf3904ea662e281393e80640a536af5a
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-09 07:55:41 +0000

    Update bert.py

--- bert.py
+++ bert.py
@@ -153,7 +153,8 @@ class Ner:
         #make groups of words that model finds similar
         #for amount of labels (labels after sep) make a section that prints all words with that label
         print(words)
-        sep_pos = words.index('[SEP]') # need to find position of seperator, mask ids?
+        print(input_mask)
+        sep_pos = input.index('[SEP]') # need to find position of seperator, mask ids?
         print("sep pos: " + str(sep_pos))
         before_sep = labels[:sep_pos]
         after_sep = labels[sep_pos+1:len(words)]
