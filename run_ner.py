9affa52d1f114b5f3508a0e861d438ca828a3ebc
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-03-14 23:37:55 +0000

    running with actual labels

--- run_ner.py
+++ run_ner.py
@@ -227,17 +227,16 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
         labellist.append("O")
 
         # "actual" default for labels
-        #for label in simp_labs:
-            #label_to_add = list(simplified_labels.keys())[list(simplified_labels.values()).index(label)]
-            #label_in_string = str(label_to_add)
-            #labellist.append(label_in_string)
+        for label in simp_labs:
+            label_to_add = list(simplified_labels.keys())[list(simplified_labels.values()).index(label)]
+            label_in_string = str(label_to_add)
+            labellist.append(label_in_string)
         #print(labellist)
         
         # "O" default for labels
-        for label in simp_labs:
-            labellist.append("O")
-        # print(labellist)
-        # print(textlist)
+        #for label in simp_labs:
+        #    labellist.append("O")
+
 
         tokens = []
         labels = []
