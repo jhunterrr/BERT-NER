61b17649bc74d61eb609fc39e3a989d0283a96dd
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-04-07 14:00:38 +0100

    bugs

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ print(output)
 
 #New method
 print("Group 4: ZER Test")
-model.predict_zero_shot(path, ["person", "location", "organisation"])
+evaluate_zero_shot(path, ["person", "location", "organisation"])
 
 
 def evaluate_zero_shot(filename: str, label_list: list):
@@ -52,9 +52,7 @@ def evaluate_zero_shot(filename: str, label_list: list):
                 if line.isspace() and text != []:
                     for i, old_lab in enumerate(ground_truth):
                         ground_truth[i] = simplified_labels[old_lab.strip()]
-                    print(text)
-                    print(ground_truth)
-                    model.predict_zero_shot(
+                    model.predict_zero_shot(text, label_list, ground_truth)
                 #if not blank line, add to line, find label and assign it
                 if not line.isspace():
                     word = line.split(' ')[0]
