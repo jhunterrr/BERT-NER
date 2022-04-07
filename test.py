752162774d14205fb5ec36f99a1f3d429eb69e74
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-04-07 16:14:34 +0100

    update for original method evaluation

--- test.py
+++ test.py
@@ -71,6 +71,44 @@ print(output)
 print("Group 4: ZER Test")
 evaluate_zero_shot(path, ["person", "location", "organisation"])
 
+def evaluate_zero_shot(filename: str):
+    simplified_labels = { "O": "O", "B-MISC": "miscellaneous", "I-MISC": "miscellaneous", "B-PER": "person", "I-PER": "person", 
+                         "B-ORG": "organisation", "I-ORG": "organisation", "B-LOC": "location", "I-LOC": "location", "[CLS]": "[CLS]", "[SEP]": "[SEP]" } 
+    #initialise text and value for retrieving label
+    text = []
+    ground_truth = []
+    entities_selected = 0
+    entities_relevant = 0
+    true_positives = 0
+    label_loc = 4
+    #read file
+    with open(filename) as file:
+        next(file)
+        for line in file:
+                #if blank line, process method and reset sentence
+                if line.isspace() and text != []:
+                    for i, old_lab in enumerate(ground_truth):
+                        print(simplified_labels[old_lab.strip()])
+                        ground_truth[i] = simplified_labels[old_lab.strip()]
+                    print(ground_truth)
+                    tp, es, er = model.predict_original(text, ground_truth)
+                    true_positives += tp
+                    entities_selected += es
+                    entities_relevant += er
+                    text = []
+                    ground_truth = []
+                #if not blank line, add to line, find label and assign it
+                if not line.isspace():
+                    word = line.split(' ')[0]
+                    label = line.split(' ')[label_loc-1]
+                    text.append(word)
+                    ground_truth.append(label)
+        print("Recall: " + str(true_positives / entities_relevant))
+        print("Precision: " + str(true_positives / entities_selected))
+        
+model = Ner("/content/drive/MyDrive/Colab Notebooks/BERT-NER/out_base")
+evaluate_zero_shot(path)
+
 
 
 
