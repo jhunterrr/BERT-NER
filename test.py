d083d67f3c5e81b914796d7aff2c19ad53531fcf
Jamie <71834236+jhunterrr@users.noreply.github.com>
2022-04-20 23:02:50 +0100

    allowance for user to insert custom BERT models

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from bert import Ner
 import sys
 
-def evaluate_zero_shot(filename: str, label_list: list):
+def evaluate_zero_shot(filename: str, label_list=None: list):
     simplified_labels = { "O": "O", "B-MISC": "miscellaneous", "I-MISC": "miscellaneous", "B-PER": "person", "I-PER": "person", 
                          "B-ORG": "organisation", "I-ORG": "organisation", "B-LOC": "location", "I-LOC": "location", "[CLS]": "[CLS]", "[SEP]": "[SEP]" } 
     #initialise text and value for retrieving label
@@ -22,7 +22,10 @@ def evaluate_zero_shot(filename: str, label_list: list):
                         #print(simplified_labels[old_lab.strip()])
                         ground_truth[i] = simplified_labels[old_lab.strip()]
                     #print(ground_truth)
-                    tp, es, er = model.predict_zero_shot(text, label_list, ground_truth)
+                    if label_list != None:
+                        tp, es, er = model.predict_zero_shot(text, label_list, ground_truth)
+                    else:
+                        tp, es, er = model.predict_original(text, ground_truth)
                     true_positives += tp
                     entities_selected += es
                     entities_relevant += er
@@ -36,9 +39,16 @@ def evaluate_zero_shot(filename: str, label_list: list):
                         total_entities += 1
                     text.append(word)
                     ground_truth.append(label)
-        print("Accuracy: " + str(true_positives / total_entities))
-        print("Recall: " + str(true_positives / entities_relevant))
-        print("Precision: " + str(true_positives / entities_selected))
+          accuracy = true_positives / total_entities
+        recall = true_positives / entities_relevant
+        precision = true_positives / entities_selected
+        f1 = 2 * (precision * recall) / (precision + recall)
+        
+        #metric evaluation
+        print("Accuracy: " + str(accuracy))
+        print("Recall: " + str(recall))
+        print("Precision: " + str(precision))
+        print("F1 score: " + str(f1))
 
 # model = Ner(str(sys.argv))
 model = Ner("/content/drive/MyDrive/Colab Notebooks/BERT-NER/out_base_simp_shuffled/content/out_base_simp_shuffled")
@@ -65,7 +75,7 @@ print(output)
 #Locations
 print("Group 3: Locations")
 print("Expected: Final classification for person label below")
-expected = model.predict("Locations")
+expected = model.predict("location")
 output = model.predict("Berlin New York Munich Belfast Dublin England America")
 print(expected)
 print("Actual Results:")
@@ -73,52 +83,33 @@ print(output)
 
 #New method
 print("Group 4: ZER Test")
-print("EVALUATION: OUR BERT METHOD")
-evaluate_zero_shot(path, ["person", "location", "organisation"])
-
-def evaluate_zero_shot(filename: str):
-    simplified_labels = { "O": "O", "B-MISC": "miscellaneous", "I-MISC": "miscellaneous", "B-PER": "person", "I-PER": "person", 
-                         "B-ORG": "organisation", "I-ORG": "organisation", "B-LOC": "location", "I-LOC": "location", "[CLS]": "[CLS]", "[SEP]": "[SEP]" } 
-    #initialise text and value for retrieving label
-    text = []
-    ground_truth = []
-    entities_selected = 0
-    entities_relevant = 0
-    true_positives = 0
-    total_entities = 0
-    label_loc = 4
-    #read file
-    with open(filename) as file:
-        next(file)
-        for line in file:
-                #if blank line, process method and reset sentence
-                if line.isspace() and text != []:
-                    for i, old_lab in enumerate(ground_truth):
-                        #print(simplified_labels[old_lab.strip()])
-                        ground_truth[i] = simplified_labels[old_lab.strip()]
-                    #print(ground_truth)
-                    tp, es, er = model.predict_original(text, ground_truth)
-                    true_positives += tp
-                    entities_selected += es
-                    entities_relevant += er
-                    text = []
-                    ground_truth = []
-                #if not blank line, add to line, find label and assign it
-                if not line.isspace():
-                    word = line.split(' ')[0]
-                    label = line.split(' ')[label_loc-1]
-                    if label.strip() != 'O':
-                        total_entities += 1
-                    text.append(word)
-                    ground_truth.append(label)
-        print("Accuracy: " + str(true_positives / total_entities))
-        print("Recall: " + str(true_positives / entities_relevant))
-        print("Precision: " + str(true_positives / entities_selected))
+#print("EVALUATION: OUR BERT METHOD")
+#evaluate_zero_shot(path, ["person", "location", "organisation"])
         
 print("EVALUATION: ORIGINAL BERT METHOD")
-model = Ner("/content/drive/MyDrive/Colab Notebooks/BERT-NER/out_base")
-evaluate_zero_shot(path)
+#model = Ner("/content/drive/MyDrive/Colab Notebooks/BERT-NER/out_base")
+#evaluate_zero_shot(path)
 
+def main():
+    parser = argparse.ArgumentParser()
+
+    ## Required parameters
+    parser.add_argument("--model_dir_orig",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="The original BERT model to be used in evaluation")
+    parser.add_argument("--model_dir_new", default=None, type=str, required=True,
+                        help="The new BERT model to be used in evaluation ")
+    
+    
+    print("EVALUATION: OUR BERT METHOD")
+    model = Ner(args.model_dir_new, ["person", "location", "organisation", "miscellaneous"])
+    evaluate_zero_shot(path)
+    
+    print("EVALUATION: ORIGINAL BERT METHOD")
+    model = Ner(args.model_dir_orig)
+    evaluate_zero_shot(path)
 
 
 
